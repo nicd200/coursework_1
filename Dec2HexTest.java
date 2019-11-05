a9a56d95143b23a050a0696221457d4ac7348fec
nicd200 <ndochn200@caledonian.ac.uk>
2019-11-05 15:15:24 +0000

    Unchanged code.

---
+++ Dec2HexTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Dec2HexTest {
+
+    @Test
+    public void TestWithCorrectInteger() {
+        Dec2Hex dec2Hex = new Dec2Hex();
+        int Arg1 = 30;
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal = "";
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+        String result = hexadecimal;
+
+        assertEquals("1E", result);
+
+    }
+}
