131c7a72fd2741b8d97a3e57216a538e3cde96bb
nicd200 <ndochn200@caledonian.ac.uk>
2019-11-06 16:57:46 +0000

    Task 5: Improve the Java project.

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -5,11 +5,10 @@ public class Dec2HexTest {
 
     @Test
     public void TestWithCorrectInteger() {
-        Dec2Hex dec2Hex = new Dec2Hex();
-        int Arg1 = 30;
+        Dec2Hex.Arg1 = 30;
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num;
-        num = Arg1;
+        num = Dec2Hex.Arg1;
         String hexadecimal = "";
         while (num != 0) {
             rem = num % 16;
